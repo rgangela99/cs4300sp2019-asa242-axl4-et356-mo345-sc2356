843dcd4b8eda929cbbf8d87a5100ebee560dfbcf
Anjelika Amog <asa242@cornell.edu>
2019-04-29 20:11:26 -0400

    last merge conflict

--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -246,7 +246,6 @@ for k in medium_ind_to_art_info.keys():
             
     medium_sentiment_scores.append(curr_score)
 
->>>>>>> 2c58e9ca607c000f4fe97f31844c5dd8a4e33467
 #search function from YouTube video to Medium article
 def mediumSearch(query,keywords,max_time):
     num_results = 10
